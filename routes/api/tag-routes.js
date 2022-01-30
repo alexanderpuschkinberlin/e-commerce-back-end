7706966eb3d5677f4a6e772a83ad6ae7c0710ace
alexanderpuschkinberlin <90645435+alexanderpuschkinberlin@users.noreply.github.com>
2022-01-30 13:03:56 +0100

    add put request for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,8 +53,19 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updateTag);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json("Error while updating category data in the DB");
+  }
 });
 
 router.delete("/:id", async (req, res) => {
